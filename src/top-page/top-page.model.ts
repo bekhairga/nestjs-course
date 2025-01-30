567b70692bc72710f9809184b72ef5851e17d1c8
Bekhzod Irgashev <irgashevbehzod@gmail.com>
2025-01-30 15:15:34 +0500

    feat: updated models to nestjs/mongoose schemas

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,26 +1,67 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
 export enum TopLevelCategory {
 	Courses,
 	Services,
 	Books,
 	Products,
 }
-export class TopPageModel {
-	_id: string;
-	firstCategory: TopLevelCategory;
-	secondCategory: string;
+
+export class TopPageAdvantage {
+	@Prop()
 	title: string;
+
+	@Prop()
+	description: string;
+}
+export class HhData {
+	@Prop()
+	count: number;
+
+	@Prop()
+	juniorSalary: number;
+
+	@Prop()
+	middleSalary: number;
+
+	@Prop()
+	seniorSalary: number;
+}
+
+export type TopPageDocument = HydratedDocument<TopPageModel>;
+
+@Schema({ timestamps: true })
+export class TopPageModel {
+	@Prop({ enum: TopLevelCategory })
+	firstCategory: TopLevelCategory;
+
+	@Prop()
+	secondCategory: string;
+
+	@Prop({ unique: true })
+	alias: string;
+
+	@Prop()
+	title: string;
+
+	@Prop()
 	category: string;
-	hh?: {
-		count: number;
-		juniorSalary: number;
-		middleSalary: number;
-		seniorSalary: number;
-	};
-	advantages: {
-		title: string;
-		description: string;
-	}[];
+
+	@Prop()
+	hh?: HhData;
+
+	@Prop([TopPageAdvantage])
+	advantages: TopPageAdvantage[];
+
+	@Prop()
 	seoText: string;
+
+	@Prop([String])
 	tags: string[];
+
+	@Prop()
 	tagsTitle: string;
 }
+
+export const TopPageSchema = SchemaFactory.createForClass(TopPageModel);
