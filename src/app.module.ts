d0bfc0cd006f15caa2124a2421ba256545674cd9
Bekhzod Irgashev <irgashevbehzod@gmail.com>
2025-01-30 12:33:23 +0500

    feat: added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-	imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+	imports: [ConfigModule.forRoot(), AuthModule, TopPageModule, ProductModule, ReviewModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
